7e4c66a8d1cdc24a4b325124e24f8d1d81c43776
kevin vergara <kevin.vergara.lda@gmail.com>
2015-10-17 17:27:46 -0300

    primer comentario

---
+++ tarea2ia/src/paquete/clasePrincipal.java
@@ -0,0 +1,14 @@
+
+package paquete;
+
+public class clasePrincipal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //hola ctm
+        
+        
+        
+    }
+    
+}
